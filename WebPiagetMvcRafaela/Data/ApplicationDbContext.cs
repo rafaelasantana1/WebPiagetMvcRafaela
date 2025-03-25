580d3d414d7f1e74a640facbe2d5137be380f120
rafaelasantana1 <rafasantosa2004@gmail.com>
2025-03-25 11:19:23 -0300

    Controles e migration database

--- WebPiagetMvcRafaela/Data/ApplicationDbContext.cs
+++ WebPiagetMvcRafaela/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebPiagetMvcRafaela.Models;
 
 namespace WebPiagetMvcRafaela.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+public DbSet<WebPiagetMvcRafaela.Models.Aluno> Aluno { get; set; } = default!;
 }
--- WebPiagetMvcRafaela/Data/Migrations/20250325141449_Piaget.Designer.cs
+++ WebPiagetMvcRafaela/Data/Migrations/20250325141449_Piaget.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebPiagetMvcRafaela.Data;
 
@@ -11,9 +12,11 @@ using WebPiagetMvcRafaela.Data;
 namespace WebPiagetMvcRafaela.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325141449_Piaget")]
+    partial class Piaget
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
